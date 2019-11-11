5f827efcf0668fd9a1942febdf142dae1dd3890c
Vítor Galvão <info@vitorgalvao.com>
2019-11-11 23:39:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# homebrew-mpv
+
+[Homebrew](http://brew.sh/) HEAD-only formula for [mpv](https://mpv.io/). Install with:
+
+```
+brew install --HEAD vitorgalvao/mpv/mpv
+```
+
+Being HEAD-only, upgrade with:
+
+```
+brew reinstall mpv
+```
---
+++ mpv.rb
@@ -0,0 +1,54 @@
+class Mpv < Formula
+  desc "Media player based on MPlayer and mplayer2"
+  homepage "https://mpv.io"
+  head "https://github.com/mpv-player/mpv.git"
+
+  depends_on "docutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python" => :build
+
+  depends_on "ffmpeg"
+  depends_on "jpeg"
+  depends_on "libarchive"
+  depends_on "libass"
+  depends_on "little-cms2"
+  depends_on "luajit"
+
+  depends_on "mujs"
+  depends_on "uchardet"
+  depends_on "vapoursynth"
+  depends_on "youtube-dl"
+
+  def install
+    # LANG is unset by default on macOS and causes issues when calling getlocale
+    # or getdefaultlocale in docutils. Force the default c/posix locale since
+    # that's good enough for building the manpage.
+    ENV["LC_ALL"] = "C"
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-html-build
+      --enable-javascript
+      --enable-libmpv-shared
+      --enable-lua
+      --enable-libarchive
+      --enable-uchardet
+      --confdir=#{etc}/mpv
+      --datadir=#{pkgshare}
+      --mandir=#{man}
+      --docdir=#{doc}
+      --zshdir=#{zsh_completion}
+    ]
+
+    system "python3", "bootstrap.py"
+    system "python3", "waf", "configure", *args
+    system "python3", "waf", "install"
+
+    system "python3", "TOOLS/osxbundle.py", "build/mpv"
+    prefix.install "build/mpv.app"
+  end
+
+  test do
+    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+  end
+end
